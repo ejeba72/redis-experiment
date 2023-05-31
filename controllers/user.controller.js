cab32c17535339912d2d0728c2173d12d0b4d91a
Emmanuel Eni <eejeba@gmail.com>
2023-05-31 20:13:52 +0100

    add 'getAllUser' and 'getOneUser' endpoints

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,28 +2,63 @@
 
 const { UserModel } = require("../models/user.model");
 
-async function postUser(req, res) {
+async function postUser(res, req) {
     try {
-        const { name, age } = req.body;
-        const payload = { name, age };
-        const newUser = new UserModel(payload);
-        // // console.log(name, age);
-        // // const newUser = new UserModel({ name, age });
+        const { name, age } = res.body;
+        console.log(name, age);
+        const newUser = new UserModel({ name, age });
         await newUser.save();
-        // console.log({
-        //     msg: 'New user saved',
-        //     newUser
-        // });
-        // res.status(201).send({
-        //     msg: 'New user saved',
-        //     newUser
-        // });
-        console.log(newUser);
-        res.send(newUser);
+        console.log({
+            msg: 'New user saved',
+            newUser
+        });
+        req.status(201).send({
+            msg: 'New user saved',
+            newUser
+        });
     } catch (err) {
         console.error(err.message);
-        res.status(500).send(err);
+        req.status(500).send(err);
     }
 }
 
-module.exports = { postUser };
+async function getAllUsers(response, request_na) {
+    try {
+        const getUsersFromDb = await UserModel.find();
+        console.log(getUsersFromDb);
+        request_na.status(200).send(getUsersFromDb);
+    } catch (err) {
+        console.error(err.message);
+        request_na.status(500).send(err.message);
+    }
+}
+
+async function getOneUser(req, res) {
+    try {
+        const { age } = req.params;
+        console.log('{ req.params: ', age, ' }');
+        const user = await UserModel.findOne({ age });
+        console.log({ user });
+        res.status(200).send({ user });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send(err.message);
+    }
+}
+
+module.exports = { postUser, getAllUsers, getOneUser };
+
+
+
+// async function getOneUser(odion, kenny) {
+//     try {
+//         const { age } = odion.params;
+//         console.log({ age });
+//         const user = await UserModel.findOne({ age });
+//         console.log(user);
+//         kenny.status(200).send(user);
+//     } catch (err) {
+//         console.error(err.message);
+//         kenny.status(500).send(err.message);
+//     }
+// }
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,12 @@
 // 1737hr wed may31 2023
 
 const { Router } = require("express");
-const { postUser } = require("../controllers/user.controller");
+const { postUser, getAllUsers, getOneUser } = require("../controllers/user.controller");
 
 const router = Router();
 
-router.post('/', postUser);
+// router.post('/', postUser);
+router.route('/').post(postUser).get(getAllUsers)
+router.get('/:age', getOneUser);
 
 module.exports = { userRoute: router };
